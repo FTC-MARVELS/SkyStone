87350561abe211822a176762cab3571aaa263748
Tim Vargo <tim.g.vargo@gmail.com>
2019-12-14 12:32:19 -0700

    Add files via upload
    Init color sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SeekSkystones__au.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SeekSkystones__au.java
@@ -6,6 +6,8 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
@@ -25,8 +27,14 @@ public class SeekSkystones__au extends LinearOpMode {
   /**
    * This function is executed when this Op Mode is selected from the Driver Station.
    */
+
+  ColorSensor sensorColor;
+  DistanceSensor sensorDistance;
+
   @Override
   public void runOpMode() {
+  
+    // initialization
     PullServoAsServo = hardwareMap.servo.get("PullServoAsServo");
     PullServo2AsServo = hardwareMap.servo.get("PullServo2AsServo");
     FrontDriveMotorAsDcMotor = hardwareMap.dcMotor.get("FrontDriveMotorAsDcMotor");
@@ -34,7 +42,21 @@ public class SeekSkystones__au extends LinearOpMode {
     LeftDriveMotorAsDcMotor = hardwareMap.dcMotor.get("LeftDriveMotorAsDcMotor");
     BackDriveMotorAsDcMotor = hardwareMap.dcMotor.get("BackDriveMotorAsDcMotor");
 
-    // initialization
-    
+    // get a reference to the color sensor.
+    sensorColor = hardwareMap.get(ColorSensor.class, "sensor_color_distance");
+
+    // get a reference to the distance sensor that shares the same name.
+    sensorDistance = hardwareMap.get(DistanceSensor.class, "sensor_color_distance");
+
+    // hsvValues is an array that will hold the hue, saturation, and value information.
+    float hsvValues[] = {0F, 0F, 0F};
+
+    // values is a reference to the hsvValues array.
+    final float values[] = hsvValues;
+
+    // sometimes it helps to multiply the raw RGB values with a scale factor
+    // to amplify/attentuate the measured values.
+    final double SCALE_FACTOR = 255;
+
   }
 }
