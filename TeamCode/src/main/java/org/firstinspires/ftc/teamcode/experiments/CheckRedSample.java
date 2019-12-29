7d8503c8002d00b6f4d897eec00b3389f8d4942b
Tim Vargo <tim.g.vargo@gmail.com>
2019-12-29 13:29:28 -0700

    Add files via upload
    moving some files into "experiments" from parent folder, committing others for the first time.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/experiments/CheckRedSample.java
@@ -0,0 +1,79 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.graphics.Color;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+import org.firstinspires.ftc.robotcore.external.JavaUtil;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+@TeleOp(name = "CheckRedSample (Blocks to Java)", group = "")
+public class CheckRedSample extends LinearOpMode {
+
+  private ColorSensor sensorColorRange;
+
+  /**
+   * This function is executed when this Op Mode is selected from the Driver Station.
+   */
+  @Override
+  public void runOpMode() {
+    int colorHSV;
+    float hue;
+    float sat;
+    float val;
+
+    sensorColorRange = hardwareMap.colorSensor.get("sensorColorRange");
+
+    // This op mode demonstrates the color and distance features of the REV sensor.
+    telemetry.addData("CheckRed Op Mode", "Press start to continue...");
+    telemetry.update();
+    waitForStart();
+    if (opModeIsActive()) {
+      // Put run blocks here.
+      while (opModeIsActive()) {
+        // Display distance info.
+        telemetry.addData("Dist to tgt (cm)", ((DistanceSensor) sensorColorRange).getDistance(DistanceUnit.CM));
+        // Display reflected light.
+        telemetry.addData("Light detected", ((OpticalDistanceSensor) sensorColorRange).getLightDetected());
+        // Convert RGB values to HSV color model.
+        // See https://en.wikipedia.org/wiki/HSL_and_HSV for details on HSV color model.
+        colorHSV = Color.argb(sensorColorRange.alpha(), sensorColorRange.red(), sensorColorRange.green(), sensorColorRange.blue());
+        // Get hue.
+        hue = JavaUtil.colorToHue(colorHSV);
+        telemetry.addData("Hue", hue);
+        // Get saturation.
+        sat = JavaUtil.colorToSaturation(colorHSV);
+        telemetry.addData("Saturation", sat);
+        // Get value.
+        val = JavaUtil.colorToValue(colorHSV);
+        telemetry.addData("Value", val);
+        // Use hue to determine if it's red, green, blue, etc..
+        if (hue < 30) {
+          telemetry.addData("Color", "Red");
+        } else if (hue < 60) {
+          telemetry.addData("Color", "Orange");
+        } else if (hue < 90) {
+          telemetry.addData("Color", "Yellow");
+        } else if (hue < 150) {
+          telemetry.addData("Color", "Green");
+        } else if (hue < 225) {
+          telemetry.addData("Color", "Blue");
+        } else if (hue < 350) {
+          telemetry.addData("Color", "purple");
+        } else {
+          telemetry.addData("Color", "Red");
+        }
+        // Check to see if it might be black or white.
+        if (sat < 0.2) {
+          telemetry.addData("Check Sat", "Is surface white?");
+        }
+        telemetry.update();
+        if (val < 0.16) {
+          telemetry.addData("Check Val", "Is surface black?");
+        }
+      }
+    }
+  }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/experiments/Fndtn_In_Against_Wall__au.java
@@ -0,0 +1,74 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@Autonomous(name = "Fndtn_In_Against_Wall", group = "")
+public class Fndtn_In_Against_Wall__au extends LinearOpMode {
+
+  private Servo PullServoAsServo;
+  private Servo PullServo2AsServo;
+  private DcMotor FrontDriveMotorAsDcMotor;
+  private DcMotor RightDriveMotorAsDcMotor;
+  private DcMotor LeftDriveMotorAsDcMotor;
+  private DcMotor BackDriveMotorAsDcMotor;
+
+  /**
+   * This function is executed when this Op Mode is selected from the Driver Station.
+   */
+  @Override
+  public void runOpMode() {
+    PullServoAsServo = hardwareMap.servo.get("PullServoAsServo");
+    PullServo2AsServo = hardwareMap.servo.get("PullServo2AsServo");
+    FrontDriveMotorAsDcMotor = hardwareMap.dcMotor.get("FrontDriveMotorAsDcMotor");
+    RightDriveMotorAsDcMotor = hardwareMap.dcMotor.get("RightDriveMotorAsDcMotor");
+    LeftDriveMotorAsDcMotor = hardwareMap.dcMotor.get("LeftDriveMotorAsDcMotor");
+    BackDriveMotorAsDcMotor = hardwareMap.dcMotor.get("BackDriveMotorAsDcMotor");
+
+    // Put initialization blocks here.
+    PullServoAsServo.setPosition(0.2);
+    PullServo2AsServo.setPosition(0.9);
+    FrontDriveMotorAsDcMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+    RightDriveMotorAsDcMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+    LeftDriveMotorAsDcMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+    BackDriveMotorAsDcMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+    RightDriveMotorAsDcMotor.setPower(0.5);
+    FrontDriveMotorAsDcMotor.setPower(0.5);
+    sleep(1500);
+    RightDriveMotorAsDcMotor.setPower(0);
+    FrontDriveMotorAsDcMotor.setPower(0);
+    sleep(100);
+    PullServoAsServo.setPosition(0.9);
+    PullServo2AsServo.setPosition(0.1);
+    sleep(1000);
+    RightDriveMotorAsDcMotor.setPower(-0.5);
+    FrontDriveMotorAsDcMotor.setPower(-0.5);
+    sleep(2550);
+    RightDriveMotorAsDcMotor.setPower(0);
+    FrontDriveMotorAsDcMotor.setPower(0);
+    sleep(100);
+    PullServoAsServo.setPosition(0.2);
+    PullServo2AsServo.setPosition(0.9);
+    sleep(1000);
+    LeftDriveMotorAsDcMotor.setPower(-0.5);
+    BackDriveMotorAsDcMotor.setPower(-0.5);
+    sleep(2000);
+    LeftDriveMotorAsDcMotor.setPower(0);
+    BackDriveMotorAsDcMotor.setPower(0);
+    sleep(200);
+    RightDriveMotorAsDcMotor.setPower(0.5);
+    FrontDriveMotorAsDcMotor.setPower(0.5);
+    sleep(750);
+    RightDriveMotorAsDcMotor.setPower(0);
+    FrontDriveMotorAsDcMotor.setPower(0);
+    sleep(200);
+    LeftDriveMotorAsDcMotor.setPower(0.5);
+    BackDriveMotorAsDcMotor.setPower(0.5);
+    sleep(2250);
+    LeftDriveMotorAsDcMotor.setPower(0);
+    BackDriveMotorAsDcMotor.setPower(0);
+  }
+}
